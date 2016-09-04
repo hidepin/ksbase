132ce31f43a8c736ecf0c90d39d5ec9f0fef7371
Hidetoshi Imai <hidepin@gmail.com>
2016-09-04 21:31:21 +0900

    apply docker-compose.yml

--- ksbase/docker-entrypoint.sh
+++ ksbase/docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 for i in /opt/os/iso/*.iso
 do
     path=`basename $i | sed 's/.iso//'`
-    /bin/mkdir -p /opt/ks/os/${path}
-    /usr/bin/mount -r -o loop -t iso9660 ${i} /opt/ks/os/${path}
+    /bin/mkdir -p /opt/docker/ks/os/${path}
+    /usr/bin/mount -r -o loop -t iso9660 ${i} /opt/docker/ks/os/${path}
 done
 
 exec /usr/sbin/httpd -D FOREGROUND
